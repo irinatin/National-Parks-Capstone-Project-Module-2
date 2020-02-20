4d60652c77cb03ee4ed33f603a297cf339af1399
Reed Bentley <reed.bentley@gmail.com>
2020-02-20 12:10:36 -0500

    Campgrounds creation.

---
+++ src/main/java/com/techelevator/Campgrounds.java
@@ -0,0 +1,56 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public class Campgrounds {
+
+	private Long id;
+	private String name;
+	private String location;
+	private int openFromMonth;
+	private int openToMonth;
+	private BigDecimal dailyFee;
+
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public int getOpenFromMonth() {
+		return openFromMonth;
+	}
+	public void setOpenFromMonth(int openFromMonth) {
+		this.openFromMonth = openFromMonth;
+	}
+	public int getOpenToMonth() {
+		return openToMonth;
+	}
+	public void setOpenToMonth(int openToMonth) {
+		this.openToMonth = openToMonth;
+	}
+	public BigDecimal getDailyFee() {
+		return dailyFee;
+	}
+	public void setDailyFee(BigDecimal dailyFee) {
+		this.dailyFee = dailyFee;
+	}
+	
+	
+
+
+
+}
